17c868a4d19ad4cadcacd57c1271ad6d960af2b4
rhlk92 <rhlk92@gmail.com>
2016-08-09 16:08:18 +0530

    added format_suffix_patterns to urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns(
     'api.views',
     url(r'^reminders/$', 'reminder_list_api', name='reminder_list_api'),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
