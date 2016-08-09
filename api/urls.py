01b3bc33cd3edc2b64ebbbe9d931746564601d55
rhlk92 <rhlk92@gmail.com>
2016-08-09 19:15:05 +0530

    changed urls for class based views

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import ReminderList
 
 urlpatterns = patterns(
     'api.views',
-    url(r'^reminders/$', 'reminder_list_api', name='reminder_list_api'),
+    url(r'^reminders/$', ReminderList.as_view(), name='reminder_list'),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
