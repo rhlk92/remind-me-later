4b10de520c85d41ae44c12a1e1cb45d843234c85
rhlk92 <rhlk92@gmail.com>
2016-08-10 15:22:59 +0530

    changed views functions to viewsets

--- api/views.py
+++ api/views.py
@@ -1,19 +1,11 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .models import Reminder
 from .serializers import ReminderSerializer
 
 
-@api_view(['POST'])
-def reminder_list_api(request):
+class ReminderViewSet(ModelViewSet):
     """
     create a new reminder.
     """
-    if request.method == 'POST':
-        serializer = ReminderSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+    queryset = Reminder.objects.all()
+    serializer_class = ReminderSerializer
