b190a8bbf4d5fc8874402190ccb93736828d9ba2
rhlk92 <rhlk92@gmail.com>
2016-08-10 16:45:27 +0530

    added router to configure urls automatically

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,5 @@ router.register(r'reminders', ReminderViewSet)
 
 urlpatterns = patterns(
     '',
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls',
-        namespace='rest_framework'))
+    url('^api/', include(router.urls)),
 )
